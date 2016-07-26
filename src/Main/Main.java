ff473c99dc524cf3195d484b413b9f8d15c1324a
theblockbreaker1 <eben.rogers1@gmail.com>
2016-07-26 10:53:43 -0700

    minor Main method change

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,6 @@ package Main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(new ImportData("./data.csv"));
+		System.out.println(new ImportData("data.csv"));
 	}
 }

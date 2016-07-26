655717d7ea9c25da8ec92fe197a22725099f838b
theblockbreaker1 <eben.rogers1@gmail.com>
2016-07-26 14:46:54 -0700

    Tiny bugfix in ImportData that caused the data to be taken up wrong

--- src/Main/ImportData.java
+++ src/Main/ImportData.java
@@ -32,18 +32,19 @@ public class ImportData {
 		String[] temp = {""};
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(csvFile))){
-			if ((line = br.readLine()) !=null)
+			if ((line = br.readLine()) !=null){
 				temp = line.split(",");
 				for (String a: temp)
 					attributes.add(new Attribute(a));
-			
-			while ((line = br.readLine()) !=null)
+			}
+			while ((line = br.readLine()) !=null){
 				temp = line.split(",");
 				data.add(new Record());
 				for (int i=0; i<temp.length; i++){
 					attributes.get(i).add(temp[i]);
 					data.get(data.size()-1).add(attributes.get(i), temp[i]);
 				}
+			}
 		} catch (IOException e){
 			e.printStackTrace();
 		}

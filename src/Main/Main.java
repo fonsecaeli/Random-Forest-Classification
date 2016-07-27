834aa6c260d65dd178fac13e0f0858ca87ff09ea
theblockbreaker1 <eben.rogers1@gmail.com>
2016-07-27 13:16:33 -0700

    Moved data.cvs file

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -3,10 +3,6 @@ package Main;
 public class Main {
 
 	public static void main(String[] args) {
-		//System.out.println(new ImportData("./data.csv"));
-            
-            
-            
-                //Ben was here
+		System.out.println(ImportData.importData("data.csv"));
 	}
 }

d6de6118c51ab832606525a19f2cb84d8ff473d0
theblockbreaker1 <eben.rogers1@gmail.com>
2016-07-27 11:10:00 -0700

    added documentation to ImportData, DataSet, and Record

--- src/Main/Record.java
+++ src/Main/Record.java
@@ -5,33 +5,42 @@ import java.util.LinkedHashMap;
 
 
 public class Record{
+	/**
+	* Stores the data in a LinkedHashMap with the keys as the data types and the values as the actual values
+	*/
     private LinkedHashMap<Attribute, String> data;
-    private boolean classification; //TODO: remember set the classification at some point needs to be incorporated in ImportData.java
-
+    
+	/**
+	* Initializes data
+	*/
     public Record(){
             data = new LinkedHashMap<>();
     }
 
+	/**
+	* adds an entry to the hashmap
+	*/
     public void add(Attribute attribute, String value){
             data.put(attribute, value);
     }
-
+	
+	/**
+	* Returns the hashmap so data can be retrieved from it
+	*/
     public HashMap <Attribute, String> getData(){
             return data;
     }
 
-    public boolean getClassification() {
-		return classification;
-    }
-
+	/**
+	* Input key, get value
+	*/
     public String getValue(Attribute att) {
             return data.get(att);
     }
 
-    public void setClass(boolean b) {
-        classification = b;
-    }
-
+	/**
+	* Returns the string representation of the class
+	*/
     public String toString() {
         String str = "";
         for(Attribute att: data.keySet()) {

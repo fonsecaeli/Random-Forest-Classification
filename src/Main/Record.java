a7f9b26f38cfcac807b96af5ffd95c4b1f952747
Acniea <bq2367@gmail.com>
2016-07-27 10:03:27 -0700

    Updated Record.java to include classification variable and methods

--- src/Main/Record.java
+++ src/Main/Record.java
@@ -1,24 +1,45 @@
 package Main;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 
 public class Record{
-	private HashMap <Attribute, String> record;
-	
-	public Record(){
-		record = new HashMap <Attribute, String>();
-	}
-	
-	public void add(Attribute attribute, String data){
-		record.put(attribute, data);
-	}
-	
-	public HashMap <Attribute, String> getData(){
-		return record;
-	}
-        
-        public String getAttribute(Attribute att){
-            return record.get(att);
+    private LinkedHashMap<Attribute, String> data;
+    private boolean classification; //TODO: remember set the classification at some point needs to be incorporated in ImportData.java
+
+    public Record(){
+            data = new LinkedHashMap<>();
+    }
+
+    public void add(Attribute attribute, String value){
+            data.put(attribute, value);
+    }
+
+    public HashMap <Attribute, String> getData(){
+            return data;
+    }
+
+    public boolean getClassification() {
+		return classification;
+    }
+
+    public String getValue(Attribute att) {
+            return data.get(att);
+    }
+
+    public void setClass(boolean b) {
+        classification = b;
+    }
+
+    public String toString() {
+        String str = "";
+        for(Attribute att: data.keySet()) {
+            String key = att.toString();
+            String value = data.get(att);
+            str += key + ": " + value + ", ";
         }
+        //System.out.println(data.keySet());
+        return str;
+    }
 }

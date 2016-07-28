e836f943ad2b78013979455e0cc7cb955c1e3bf2
theblockbreaker1 <eben.rogers1@gmail.com>
2016-07-28 14:05:05 -0700

    added a toString() for the Node class so we can print out our trees

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -3,6 +3,25 @@ package com.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println(ImportData.importData("data.csv"));
+		Attribute att = new Attribute("Name");
+		att.add("eben");
+		att.add("eli");
+		Attribute att2 = new Attribute("Glasses");
+		att2.add("no");
+		att2.add("yes");
+		Attribute att3 = new Attribute("Test");
+		att3.add("testchoice1");
+		att3.add("testchoice2");
+		Node node = new Node();
+		Node node2 = new Node();
+		Node node3 = new Node();
+		node.setAttribute(att);
+		node.getChild("eben").setAttribute(att2);
+		node.getChild("eli").setAttribute(att3);
+		node.getChild("eben").getChild("no").setDecision("1");
+		node.getChild("eben").getChild("yes").setDecision("2");
+		node.getChild("eli").getChild("testchoice1").setDecision("1");
+		node.getChild("eli").getChild("testchoice2").setDecision("2");
+		System.out.println(node);
 	}
 }
--- src/com/main/Node.java
+++ src/com/main/Node.java
@@ -52,4 +52,27 @@ public class Node {
     public String getDecision(){
         return decision;
     }
+	
+	public String toString(){
+		if (decision == null)
+			return stringMaker(0, "Base Node");
+		else throw new Error("You can't print an unpopulated tree!");
+	}
+	
+	public String stringMaker(int tabs, String str){
+		String toReturn = tabs(tabs)+">>> [Option: "+str+"]";
+		if (decision == null){
+			toReturn+=", [Attribute: "+att.getName()+"]\n";
+			for (String a: children.keySet())
+				toReturn+=children.get(a).stringMaker(tabs+1, a);
+		} else toReturn+="\n"+tabs(tabs+1)+"[Classification: "+decision+"]\n";
+		return toReturn;
+	}
+	
+	public String tabs(int tabs){
+		String str = "";
+		for (int i=0; i<tabs; i++)
+			str+="\t";
+		return str;
+	}
 }

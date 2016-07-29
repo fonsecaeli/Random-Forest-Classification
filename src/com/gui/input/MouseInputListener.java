1bd2618c85f72c404012cb4ce5905da6e1ee80cd
theblockbreaker1 <eben.rogers1@gmail.com>
2016-07-29 12:01:23 -0700

    added some preliminary gui stuff

---
+++ src/com/gui/input/MouseInputListener.java
@@ -0,0 +1,34 @@
+package com.gui.input;
+
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MouseInputListener implements MouseListener{
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+
+    }
+
+}

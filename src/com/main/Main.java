cd57c1b3480db5aa56fc90aad90f5073182c2ef4
Benjamin 2 <Benjamin 2@172.30.13.109>
2016-08-01 09:14:13 -0700

    Added the screen initialization to Main.java

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,34 +1,19 @@
 package com.main;
 
+import com.gui.gfx.Screen;
 import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-            //System.out.println(ds);
-            //System.out.println(Entropy.entropy(ds));
-            /*
-            List<Attribute> atts = ds.getAttributes();
-            for(int i=0; i<atts.size(); i++){
-                System.out.println(atts.get(i).getName()+": ");
-                double attEn = Entropy.attributeEntropy(ds, atts.get(i));
-                System.out.println("Attribute Entropy: "+attEn);
-                System.out.println();
-            }*/
-            
-            /*
-            System.out.println("\n\n\n");
-            
-            ds = ImportData.importData("C:\\Users\\Benjamin 2\\Documents\\NetBeansProjects\\RandomForestClassification\\RandomForestClassification\\src\\irisData.csv");
-            System.out.println(ds);
-            System.out.println(Entropy.entropy(ds));
-            
-            atts = ds.getAttributes();
-            for(int i=0; i<atts.size(); i++){
-                System.out.println(atts.get(i).getName()+": "+Entropy.attributeEntropy(ds, atts.get(i)));
-            }
-            */
-            DataSet ds = ImportData.importData(new Scanner(System.in).nextLine().replace("\'", ""));
+            Screen screen = new Screen(640,640);
+            //For Ben: ImportData.importData("C:\\Users\\Benjamin 2\\Documents\\NetBeansProjects\\RandomForestClassification\\RandomForestClassification\\src\\tic_tac_toeData.csv");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Input your file path here and click enter: ");
+            String input = scanner.nextLine();
+            input = input.replace("\'", "").replace("\"", "");
+            DataSet ds = ImportData.importData(input);
             DecisionTree tree = new DecisionTree(ds);
             System.out.println(tree);
+            
 	}
 }

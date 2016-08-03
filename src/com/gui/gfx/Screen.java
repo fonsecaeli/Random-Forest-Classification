9d1f967ae8605f37ea1165e12735180d3ad6eba5
Acniea <bq2367@gmail.com>
2016-08-03 09:38:04 -0700

    Update Screen.java

--- src/com/gui/gfx/Screen.java
+++ src/com/gui/gfx/Screen.java
@@ -1,125 +1,45 @@
-
 package com.gui.gfx;
 
-import com.gui.input.KeyboardInputListener;
-import com.gui.input.MouseInputListener;
-import com.gui.input.MouseMotionInputListener;
-import java.awt.Canvas;
-import java.awt.Dimension;
+import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
-import javax.swing.JFrame;
+import java.awt.event.MouseEvent;
 
-public class Screen extends Canvas {
-    private int WIDTH = 512;
-    private int HEIGHT = 512;
-    private final String TITLE = "Test";
-    private BufferedImage draw;
-    private GUI gui;
-    //offsets
-    private int xoff=0;
-    private int yoff=0;
-    
-    //inputs
-    private MouseInputListener mouse;
-    private MouseMotionInputListener mouseMovement;
-    private KeyboardInputListener keyboard;
-    
-    
-    
-    public Screen(int width, int height){
-        WIDTH=width;
-        HEIGHT=height;
-        
-        setPreferredSize(new Dimension(WIDTH,HEIGHT));
-        setMaximumSize(new Dimension(WIDTH,HEIGHT));
-        setMinimumSize(new Dimension(WIDTH,HEIGHT));
-        
-        JFrame frame = new JFrame(TITLE);
-        frame.add(this);
-        frame.pack();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.setLocationRelativeTo(null);
-        frame.setVisible(true);
-        
-        init();
-    }
-    
-    public final void init() {
-        requestFocus();
-
-        draw=new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB);
-        
-    }
-    
-    public void setInput(GUI g){
-        gui = g;
-        mouse = new MouseInputListener(gui);
-        mouseMovement = new MouseMotionInputListener(gui);
-        keyboard = new KeyboardInputListener();
-        
-        this.addMouseListener(mouse);
-        this.addMouseMotionListener(mouseMovement);
-        this.addKeyListener(keyboard);
-        
-    }
-    
-    public void setXOff(int i){
-        xoff+=i;
-    }
-    
-    public void setYOff(int i){
-        yoff+=i;
-    }
-    
-    public void render(){
-        BufferStrategy bs = getBufferStrategy();
-        
-        if (bs==null){
-            createBufferStrategy(3);
-            return;
-        }
-        Graphics g = bs.getDrawGraphics();
-        
-        /*g.setColor(Color.BLACK);
-        g.fillRect(0,0,getWidth(),getHeight());*/
-
-        
-        g.drawImage(draw,0,0,null);
-        
-        g.dispose();
-        bs.show();
-        
-    }
-
-    public void drawImage(BufferedImage image, int x, int y) {
-        if(image == null){
-            return;
-        }
-        Graphics g = draw.getGraphics();
-        g.drawImage(image, x, y, null);
-        render();
-    }
-
-    
-    /**
-     * used to set offset
-     */
-    /*public void tick(){
-        int movementSpeed = 8;
-        if(keyboard.right){
-            setXOff(movementSpeed);
-        }
-        if(keyboard.left){
-            setXOff(-movementSpeed);
-        }
-        if(keyboard.up){
-            setYOff(-movementSpeed);
-        }
-        if(keyboard.down){
-            setYOff(movementSpeed);
-        }
-    }*/
+public class SideBar extends Interactable{
+	
+	private SideBar(Screen screen){
+		this(100, 0, 0,0,screen);
+	}
+	
+	public SideBar(int x, int y, int width, int height, Screen screen){
+		super(x, y, screen.getWidth()-x, screen.getHeight()-y);
+		initImage(screen);
+                addInteractable(new LoadButton(screen, this));
+                addInteractable(new SaveButton(screen, this));
+	}
+	
+	private void initImage(Screen screen){
+                Graphics g = getImage().getGraphics();
+		g.setColor(new Color(175, 175, 175));
+		g.fillRect(getX(), getY(), screen.getWidth()-getX(), screen.getHeight()-getY());
+		g.setColor(new Color(75, 75, 75));
+		g.drawLine(getX(), getY(), getX(), screen.getHeight()-1);
+	}
+	
+	private class LoadButton extends Button {
+		public LoadButton(Screen screen, SideBar sb){
+			super((((screen.getWidth()-sb.getX())/2)-(("Load...".length()*Font.getCharWidth()+Button.XBORDER)/2)+sb.getX()), 64, "Load...");
+		}
+		
+		public void mouseClicked(MouseEvent me){
+			super.mouseClicked(me);
+			System.out.println("success!");
+			
+		}
+	}
+	
+	private class SaveButton extends Button {
+		public SaveButton(Screen screen, SideBar sb){
+			super((((screen.getWidth()-sb.getX())/2)-(("Save As...".length()*Font.getCharWidth()+Button.XBORDER)/2)+sb.getX()), 128, "Save As...");
+		}
+	}
 }

2e804b3586f4f9468ded7760fe3074ac17daafeb
theblockbreaker1 <eben.rogers1@gmail.com>
2016-08-09 09:53:47 -0700

    added documentation for FishEye

--- src/com/gui/interactable/FishEye.java
+++ src/com/gui/interactable/FishEye.java
@@ -11,44 +11,58 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class FishEye extends Interactable {
+	//Background color for the FishEye window
 	private static final Color BACKGROUND_COLOR = Color.WHITE;
+	//ChildSpacind, the amount of pixels between the child nodes (bottom set of nodes)
 	private final int CHILD_SPACING = 20;
+	//Vertical spacing between node generations as a factor of the entire window, Middle height from the top as a factor of the entire window
 	private final double VERTICAL_SPACING = 0.2, MIDDLE_HEIGHT = 0.5;
         
+	//These are all of the different stacks that are used to store all of the nodes
 	ArrayList <Stack<Node>> stacks;
+	//These are all of the different trees which correspond to their respective stacks
 	DecisionTree[] trees;
+	//The current index of the tree within stacks and trees
 	private int curIndex;
+	//The top button in the tree
 	private FishEyeButton top;
+	//The central node in the tree
 	private Button middle;
+	//All the child nodes in the tree
 	private ArrayList<FishEyeButton> bottom;
+	//this flag is true if a change happened so that the render class will update everything and then set it to false
 	private boolean change;
 	
+	/**
+	* This is the constructor for the object, it stores its coordinates for the window and initializes everything
+	*/
 	public FishEye(int x, int y, int width, int height){
 		super(x, y, width, height);
 		initImage();
-		init();
+		refreshData();
 	}
 	
+	/**
+	* Initializes a background
+	*/
 	private void initImage(){
 		Graphics g = getImage().getGraphics();
 		g.setColor(BACKGROUND_COLOR);
 		g.fillRect(0, 0, getWidth(), getHeight());
 	}
 	
-	private void init(){
-		if (StaticStorage.getCurrentRandomForest()!=null){
-			trees = StaticStorage.getCurrentRandomForest().getTrees();
-			stacks = new ArrayList<>(trees.length);
-			change = false; curIndex = StaticStorage.getIndexOfCurrentTree();
-		}
-	}
-	
+	/**
+	* Makes sure the buffered image is up to date along with all the interactables stored in this object
+	*/
         @Override
 	public void render(int xOff, int yOff, Screen screen){
 		refreshData();
 		super.render(xOff, yOff, screen);
 	}
 	
+	/**
+	* Updates all of the data and redraws all of the buttons if there is a change
+	*/
 	private void refreshData(){
 		if (StaticStorage.getCurrentRandomForest()!=null&&
 			StaticStorage.getCurrentRandomForest().getTrees()!=trees){
@@ -62,6 +76,9 @@ public class FishEye extends Interactable {
 		if (StaticStorage.getCurrentRandomForest()!=null&&change) refreshImage();
 	}
 	
+	/**
+	* refreshes the entire image and changes the change boolean to false
+	*/
 	public void refreshImage(){
 		curIndex = StaticStorage.getIndexOfCurrentTree();
 		
@@ -79,6 +96,9 @@ public class FishEye extends Interactable {
 		change = false;
 	}
 	
+	/**
+	* Refreshes the top button in the tree
+	*/
 	private void refreshTop(){
 		//makes the button above the current node if the node on the stack isn't the head node and removes the button if the head node is alone on the stack
 		if (stacks.get(curIndex).size()>=2){
@@ -96,6 +116,9 @@ public class FishEye extends Interactable {
 		}
 	}
 	
+	/**
+	* Refreshes the middle button in the tree
+	*/
 	private void refreshMiddle(){
 		//'remakes' the middle button
 		Node node = stacks.get(curIndex).peek();
@@ -109,8 +132,9 @@ public class FishEye extends Interactable {
 		addInteractable(middle);
 	}
 	
-	
-	
+	/**
+	* Refreshes the child node buttons in the tree
+	*/
 	private void refreshChildren(){
 		//removes all of the child nodes so they can be 'remade'
 		if (bottom!=null)
@@ -144,6 +168,9 @@ public class FishEye extends Interactable {
 		}
 	}
 	
+	/**
+	* Draws all of the lines so that the buttons can be drawn over them later
+	*/
 	private void renderLines(){
 		if (middle!=null){
 			Graphics g = getImage().getGraphics();
@@ -158,10 +185,16 @@ public class FishEye extends Interactable {
 		}
 	}
 	
+	/**
+	* Returns true if a node is a leaf node
+	*/
 	private boolean isLeafNode(Node node){
 		return (node.getDecision()!=null);
 	}
 	
+	/**
+	* This method gets called when one of the buttons gets pressed and handles the button number accordingly
+	*/
 	public void clicked(int index){
 		if (index<0){
 			stacks.get(curIndex).pop();
@@ -174,10 +207,20 @@ public class FishEye extends Interactable {
 		}
 	}
 	
+	/**
+	* This is a button used only by fishEye that basically has some extra methods that it needs to operate
+	*/
 	public class FishEyeButton extends Button{
+		//dad is the object that contains this button object
 		private FishEye dad;
+		//index is -1 if it is the top button and 0+ if it is a child button
 		private int index;
+		//This is the key for the corresponding node within the tree
 		private String key;
+		
+		/**
+		* This constructor makes the interactable using the input text and stores all the other data in the variables
+		*/
 		public FishEyeButton(int x, int y, FishEye dad, int index, String text, String key){
 			super(x, y, text);
 			this.dad = dad;
@@ -185,10 +228,16 @@ public class FishEye extends Interactable {
 			this.key = key;
 		}
 		
+		/**
+		* Pretty self explainatory
+		*/
 		public String getKey(){
 			return key;
 		}
 		
+		/**
+		* This is what calls clicked and gives it the index of the button stored within the object
+		*/
                 @Override
 		public void onAction(MouseEvent me){
 			dad.clicked(index);
